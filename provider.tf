a90400971f3ff33a7e684a172788c640386f13ac
Venkynarla <108213954+Venkynarla@users.noreply.github.com>
2022-11-15 18:07:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -41,12 +41,11 @@ resource "aws_security_group" "security-tf" {
     Name = "security-tf"
   }
 }
-resource "aws_instance" "ubuntu" {
-  ami = "ami-08c40ec9ead489470" #us-east-1
+resource "aws_instance" "ubuntu"{
+  ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   security_groups =["aws_security_group.security.tf.name"]
    key_name = "my_key"
-  
   tags = {
     Name ="test-terra"
   }

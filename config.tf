38a525eafc0ffe514c515ffa8932e2aaa00c344e
Venkynarla <108213954+Venkynarla@users.noreply.github.com>
2022-11-19 13:54:59 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -97,7 +97,7 @@ resource "aws_instance" "jenkins-node" {
   instance_type               = "t2.micro"
   vpc_security_group_ids      = ["${aws_security_group.task-sg.id}"]
   subnet_id                   = aws_subnet.terraform-subnet.id
-  key_name                    = "new_key"
+  key_name                    = "task-1"
   count                       = 1
   associate_public_ip_address = true
   tags = {
@@ -109,7 +109,7 @@ resource "aws_instance" "deploy-node" {
   instance_type               = "t2.micro"
   vpc_security_group_ids      = ["${aws_security_group.task-sg.id}"]
   subnet_id                   = aws_subnet.terraform-subnet.id
-  key_name                    = "new_key"
+  key_name                    = "task-1"
   count                       = 1
   associate_public_ip_address = true
   tags = {

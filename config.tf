5cc0fb481bbcc78b60882043de734425cc62200b
Venkynarla <108213954+Venkynarla@users.noreply.github.com>
2022-11-19 17:29:37 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -57,7 +57,7 @@ output "aws_security_gr_id" {
 }
 
 ## Create Subnets ##
-resource "aws_subnet" "terraform_pub_subnet" {
+resource "aws_subnet" "terraform_subnet" {
   vpc_id            = aws_vpc.terraform-vpc.id
   cidr_block        = "10.0.0.0/24"
   map_public_ip_on_launch  = "true"

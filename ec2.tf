c5245c4add611d15221fa7683207a4f3670ab7d7
Venkynarla <108213954+Venkynarla@users.noreply.github.com>
2022-11-20 10:16:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,24 @@
-
+resource "aws_instance" "jenkins-node" {
+  ami                         = "ami-0b0dcb5067f052a63"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = ["${aws_security_group.task-sg.id}"]
+  subnet_id                   = aws_subnet.terraform_subnet.id
+  key_name                    = "task-1"
+  count                       = 1
+  associate_public_ip_address = true
+  tags = {
+    Name        = "jenkins-node"
+  }
+}
+resource "aws_instance" "deploy-node" {
+  ami                         = "ami-0b0dcb5067f052a63"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = ["${aws_security_group.task-sg.id}"]
+  subnet_id                   = aws_subnet.terraform_subnet.id
+  key_name                    = "task-1"
+  count                       = 1
+  associate_public_ip_address = true
+  tags = {
+    Name        = "deploy-node"
+  }
+}
